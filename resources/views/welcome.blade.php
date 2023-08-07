9f51357381b544fdc62b981afc372df08f250b8d
paulinhosjb <cdosjb@gmail.com>
2023-08-06 22:31:21 -0300

    header and banner changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,14 @@
 
 @include('structures.header')
 
-<h1 class="test">
-    Hello world!
+<h1 class="test hidden md:flex">
+    Here your money has voice
 </h1>
+
+<h1 class="test flex md:hidden">
+    Here your money has voices
+</h1>
+
 
 @include('structures.footer')
 
